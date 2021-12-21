30b36dba817dea271f1092eb1c7f963f85b3b8fd
gamedev-beja <bejarino@gmail.com>
2021-12-21 11:59:15 -0700

    Submitted Project
    Light Temple

---
+++ README.md
@@ -0,0 +1,3 @@
+# TempleLightReal
+
+Developed with Unreal Engine 4
---
+++ Source/TempleLightReal/TempleLightRealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TempleLightRealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEMPLELIGHTREAL_API ATempleLightRealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
